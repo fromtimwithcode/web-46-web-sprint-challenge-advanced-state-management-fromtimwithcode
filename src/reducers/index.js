765f9f0b9d47c6131c509926d9f0af011dff1b7d
Tim Marchant <fromtimwithcode@gmail.com>
2021-09-10 13:42:38 -0500

    Added reducer cases

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,51 @@
 
+import { FETCH_START, FETCH_SUCCESS, FETCH_FAIL, ADD_SMURF, ADD_ERROR } from '../actions/index';
+
 export const initialState = {
+    smurfs: [],
+    isLoading: false,
+    error: ''
 }
 
-const reducer = ()=>{
+const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        case FETCH_START:
+            return {
+                ...state,
+                isLoading: true,
+                error: ''
+            };
+
+        case FETCH_SUCCESS:
+            return(state);
+
+        case FETCH_FAIL:
+            return {
+                ...state,
+                smurfs: [],
+                isLoading: false,
+                error: action.payload
+            };
+
+        case ADD_SMURF:
+            return {
+                ...state,
+                smurfs: [...state.smurfs, action.payload],
+                isLoading: false,
+                error: ''
+            };
+
+        case ADD_ERROR:
+            return {
+                ...state,
+                smurfs: [...state.smurfs],
+                isLoading: false,
+                error: action.payload
+            };
+
+        default:
+            return(state);
+    };
 }
 
 //**************DO NOT EDIT ANY CODE BEYOND THIS POINT**************//
